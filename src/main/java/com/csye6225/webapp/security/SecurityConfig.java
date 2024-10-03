09ee77661492847995506e067aca311ca0684b9a
AmitAnveri <anveri.a@northeastern.edu>
2024-10-02 21:22:25 -0400

    Added CustomUserDetailsService and User auth

--- src/main/java/com/csye6225/webapp/security/SecurityConfig.java
+++ src/main/java/com/csye6225/webapp/security/SecurityConfig.java
@@ -1,31 +1,46 @@
 package com.csye6225.webapp.security;
 
+import com.csye6225.webapp.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService(); // CustomUserDetailsService will load user by email
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                // Disable CSRF for APIs
-                .csrf(csrf -> csrf.disable())
-
-                // Configure the authorization rules
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/v1/user", "/healthz").permitAll()  // Public APIs
-                                .anyRequest().authenticated()  // All other APIs require authentication
-                )
-
-                // Enable Basic Authentication
-                .httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/v1/user/self").authenticated() // Protect the GET and POST user API
+                        .anyRequest().permitAll() // Allow public access to other APIs
+                ).httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(customAuthenticationEntryPoint));
 
         return http.build();
     }
-
 }
