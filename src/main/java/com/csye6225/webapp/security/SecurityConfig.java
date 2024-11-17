59734560d159f1998016b2d39f0f3cf1e5ac0351
AmitAnveri <anveri.a@northeastern.edu>
2024-11-17 16:54:46 -0500

    Filter for unverified users

--- src/main/java/com/csye6225/webapp/security/SecurityConfig.java
+++ src/main/java/com/csye6225/webapp/security/SecurityConfig.java
@@ -1,20 +1,26 @@
 package com.csye6225.webapp.security;
 
+import com.csye6225.webapp.repository.UserRepository;
 import com.csye6225.webapp.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired
     private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -36,10 +42,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/v1/user/self", "/v1/user/self/pic").authenticated() // Protect the GET and POST user API
-                .anyRequest().permitAll() // Allow public access to other APIs
-        ).httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(customAuthenticationEntryPoint));
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/v1/user/self", "/v1/user/self/pic").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .addFilterAfter(new EmailVerificationFilter(userRepository), SecurityContextHolderAwareRequestFilter.class)
+                .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(customAuthenticationEntryPoint));
 
         return http.build();
     }
+
 }
