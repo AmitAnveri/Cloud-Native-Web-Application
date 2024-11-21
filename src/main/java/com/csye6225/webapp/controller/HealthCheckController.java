27b1226ae11e9f8a4067b260451990223798076d
AmitAnveri <anveri.a@northeastern.edu>
2024-11-21 12:28:20 -0500

    Revert copy of healthz

--- src/main/java/com/csye6225/webapp/controller/HealthCheckController.java
+++ src/main/java/com/csye6225/webapp/controller/HealthCheckController.java
@@ -49,30 +49,4 @@ public class HealthCheckController {
         }
     }
 
-    @GetMapping(path = "/cicd")
-    public ResponseEntity<Void> healthCheckCi(HttpServletRequest request) {
-        // Start the timer for this API
-        long start = System.currentTimeMillis();
-
-        statsDClient.incrementCounter("api.healthz.call_count");
-
-        if (request.getContentLength() > 0 || request.getQueryString() != null) {
-            long duration = System.currentTimeMillis() - start;
-            statsDClient.recordExecutionTime("api.healthz.time", duration);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-
-        boolean isDbConnected = healthCheckService.isDatabaseConnected();
-
-        long duration = System.currentTimeMillis() - start;
-        statsDClient.recordExecutionTime("api.healthz.time", duration);
-
-        if (isDbConnected) {
-            return ResponseEntity.ok().header("Cache-Control", "no-cache").build();
-        } else {
-            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-                    .header("Cache-Control", "no-cache")
-                    .build();
-        }
-    }
 }
